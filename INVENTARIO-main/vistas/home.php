4d8001bab45236167cf3a5579c537d0c9173f1ce
pati1703 <147661528+pati1703@users.noreply.github.com>
2023-10-11 15:12:54 -0500

    Add files via upload

---
+++ INVENTARIO-main/vistas/home.php
@@ -0,0 +1,4 @@
+<div class="container is-fluid">
+	<h1 class="title">INVENTARIO I.E. MARIA AUXILIADORA</h1>
+	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+</div>
